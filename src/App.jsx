68e2e0f5b72a1ce4ca5eb6460a079ee4cd9c9a4c
Stanko Jelisic <stanko.jelisic@issgovernance.com>
2022-11-04 14:49:23 -0400

    Sample navigation screens.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,63 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-
-const App = () => {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>Works!</Text>
+    <View>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    flex: 1,
-  },
-  paragraph: {
-    textAlign: "center",
-  },
-});
+function DetailsScreen({navigation}) {
+    return (
+          <View>
+            <Text>Details Screen</Text>
+      <Button
+        title="Go to About"
+        onPress={() => navigation.navigate('About')}
+      />
+          </View>
+        );
+}
+
+function AboutScreen({navigation}) {
+    return (
+          <View>
+            <Text>About Screen</Text>
+          </View>
+        );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  const linking = {
+      prefixes: ['https://mychat.com', 'mychat://'],
+      config: {
+              screens: {
+                        Home: '/',
+                Details: "/details",
+                About: "/about",
+                      },
+            },
+    };
+
+  return (
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
